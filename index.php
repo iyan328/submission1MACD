91ef58dd6ddb4886e42dc44ca5a619a7ecb14cb6
iyan328 <iyanprasetya328@gmail.com>
2019-05-08 16:52:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
  <head>
- <Title>Registration Form</Title>
+ <Title>Student Form</Title>
  <style type="text/css">
  	body { background-color: #fff; border-top: solid 10px #000;
  	    color: #333; font-size: .85em; margin: 20; padding: 20;
@@ -16,20 +16,20 @@
  </style>
  </head>
  <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+ <h1>Register Form</h1>
+ <p>Fill in your name, address, gender, then click <strong>Submit</strong> to register.</p>
  <form method="post" action="index.php" enctype="multipart/form-data" >
        Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
+       Address <input type="text" name="address" id="address"/></br></br>
+       Gender <input type="text" name="gender" id="gender"/></br></br>
        <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
+       <input type="submit" name="show_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "macdsubmission1.database.windows.net";
+    $user = "firchan";
+    $pass = "Sagasalare29";
+    $db = "MACDsubmission1";
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
@@ -39,43 +39,43 @@
     if (isset($_POST['submit'])) {
         try {
             $name = $_POST['name'];
-            $email = $_POST['email'];
-            $job = $_POST['job'];
+            $address = $_POST['address'];
+            $gender = $_POST['gender'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+            $sql_insert = "INSERT INTO Student (name, address, gender, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
-            $stmt->bindValue(2, $email);
-            $stmt->bindValue(3, $job);
+            $stmt->bindValue(2, $address);
+            $stmt->bindValue(3, $gender);
             $stmt->bindValue(4, $date);
             $stmt->execute();
         } catch(Exception $e) {
             echo "Failed: " . $e;
         }
-        echo "<h3>Your're registered!</h3>";
-    } else if (isset($_POST['load_data'])) {
+        echo "<h3>Registered Successfully!</h3>";
+    } else if (isset($_POST['show_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registration";
+            $sql_select = "SELECT * FROM Student";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
+                echo "<h2>Student who are registered:</h2>";
                 echo "<table>";
                 echo "<tr><th>Name</th>";
-                echo "<th>Email</th>";
-                echo "<th>Job</th>";
+                echo "<th>Address</th>";
+                echo "<th>Gender</th>";
                 echo "<th>Date</th></tr>";
                 foreach($registrants as $registrant) {
                     echo "<tr><td>".$registrant['name']."</td>";
-                    echo "<td>".$registrant['email']."</td>";
-                    echo "<td>".$registrant['job']."</td>";
+                    echo "<td>".$registrant['address']."</td>";
+                    echo "<td>".$registrant['gender']."</td>";
                     echo "<td>".$registrant['date']."</td></tr>";
                 }
                 echo "</table>";
             } else {
-                echo "<h3>No one is currently registered.</h3>";
+                echo "<h3>No student is registered.</h3>";
             }
         } catch(Exception $e) {
             echo "Failed: " . $e;
